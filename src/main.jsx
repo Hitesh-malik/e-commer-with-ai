3d8c04c0c8017679bbd6c84d5c5f8f1121abfc0b
Hitesh-malik <malik.hitu90@gmail.com>
2025-12-23 18:30:17 +0530

    feat: update API base URL and add react-hot-toast for notifications

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { CartProvider } from "./context/CartContext.jsx";
-
+import { Toaster } from "react-hot-toast";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Toaster position="top-right" />
     <ThemeProvider>
       <CartProvider>
         <BrowserRouter>
